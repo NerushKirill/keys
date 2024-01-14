014585391401427efb222048e230c0ce506c8ca6
Kirill Nerush <97055285+NerushKirill@users.noreply.github.com>
2024-01-14 10:50:20 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ -f /etc/os-release ]; then
+    source /etc/os-release
+    if [[ $NAME == "Ubuntu" ]]; then
+        echo "*** Ubuntu ***"
+        sudo apt update
+        sudo apt install -y htop mc
+    elif [[ $NAME == "CentOS Linux" ]]; then
+        echo "*** CentOS ***"
+        sudo yum update
+        sudo yum install -y htop mc
+    else
+        echo "ERROR, check your distribution Linux"
+        exit 1
+    fi
+else
+    echo "ERROR, check your distribution Linux"
+    exit 1
+fi
+
+read -p "Enter your new username: " user_name
+
+sudo useradd -m -s /bin/bash $user_name
+echo "$user_name:01234567" | sudo chpasswd
+
+sudo cp -r /home/user/new_serv/.ssh /home/$user_name/
+
+sudo chmod 700 /home/$user_name/.ssh
+sudo chmod 600 /home/$user_name/.ssh/authorized_keys
+sudo chown $user_name:$user_name /home/$user_name/.ssh
+
+echo "DONE"
